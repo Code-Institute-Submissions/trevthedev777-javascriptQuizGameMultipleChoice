58964747d0e7f1eff49e2fca245c11fae2832d46
trevthedev777 <bloodsportrecords@gmail.com>
2022-03-02 13:43:17 +0000

    add javascript global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+// global variables
+const questions = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const porgressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
